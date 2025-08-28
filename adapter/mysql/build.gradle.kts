c00776cba374d1851307985b73ff366e725d8b34
astor-dev <orangnlp@gmail.com>
2025-08-28 13:13:11 +0900

    build: mysql 모듈 셋업

---
+++ adapter/mysql/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("java-library")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.5")
+    implementation("mysql:mysql-connector-java:8.0.33")
+}
