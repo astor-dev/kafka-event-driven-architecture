8fcae8600cddc9316d2e202f6762299856b2c2fc
astor-dev <orangnlp@gmail.com>
2025-08-28 16:15:42 +0900

    feat: 쿠폰 도메인 모델

---
+++ domain/src/main/kotlin/com/astordev/ugc/coupon/model/Coupon.kt
@@ -0,0 +1,30 @@
+package com.astordev.ugc.coupon.model
+
+import com.astordev.ugc.post.model.Post
+import com.astordev.ugc.post.model.PostId
+import com.astordev.ugc.user.model.UserId
+import java.time.LocalDateTime
+
+class Coupon private constructor (
+    var id: CouponId,
+    var userId: UserId,
+    var couponEventId: CouponEventId,
+    var issuedAt: LocalDateTime,
+    var usedAt: LocalDateTime?
+) {
+    companion object {
+        fun generate(userId: UserId, couponEventId: CouponEventId): Coupon {
+            val now = LocalDateTime.now()
+            return Coupon(CouponId.generate(), userId, couponEventId, now, null);
+        }
+
+        fun fron(id: CouponId, userId: UserId, couponEventId: CouponEventId, issuedAt: LocalDateTime, usedAt: LocalDateTime?): Coupon {
+            return Coupon(id, userId, couponEventId, issuedAt, usedAt);
+        }
+    }
+
+    fun use(): Coupon {
+        this.usedAt = LocalDateTime.now();
+        return this;
+    }
+}
