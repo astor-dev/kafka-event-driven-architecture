7a10bce6783df4cbb58608199b11f3a46d2b2b05
astor-dev <orangnlp@gmail.com>
2025-11-25 17:18:26 +0900

    build: add dependency arrow-kt:2.2.0

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
     api("com.fasterxml.jackson.core:jackson-databind:2.15.0")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.3")
     api("com.fasterxml.jackson.module:jackson-module-kotlin")
+    api("io.arrow-kt:arrow-core:2.2.0")
 }
